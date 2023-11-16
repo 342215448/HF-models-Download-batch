21e39ab59834880661e80e8642f35786ff490af7
FengPeanut <67146399+342215448@users.noreply.github.com>
2023-11-16 16:48:19 +0800

    Update setup.py
    bug fixed

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name="sao_download",
+    name="niu_download",
     version="0.0.1",
     author="Peinan Feng & Junhao Ruan & Peizhuo Liu (NEUNLPLAB)",
     author_email="fpnan@foxmail@foxmail.com",
     description="A tool that can download Hugging Face model(s)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/342215448/SaoDownload.git",
     project_urls={
         "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
     },
